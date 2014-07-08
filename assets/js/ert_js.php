25a20b3bb92b2016473f4febb5af747eae938c3a
oscitas <oscitas@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-07-08 07:11:54 +0000

    = 2.6 =
    * [Fixes] fixed the undefined index ert_css & ert_js error message issue.
    
    git-svn-id: https://plugins.svn.wordpress.org/easy-responsive-tabs/trunk@944740 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- assets/js/ert_js.php
+++ assets/js/ert_js.php
@@ -2,6 +2,9 @@
 session_start();
 header("Content-type: text/javascript");
 echo 'jQuery(document).ready(function() {';
-echo $_SESSION['ert_js'];unset($_SESSION['ert_js']);
+if (isset($_SESSION['ert_js'])) {
+    echo  $_SESSION['ert_js'];
+    unset($_SESSION['ert_js']);
+}
 echo '});';
 ?>
